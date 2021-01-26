854f17ce21ee10539ee79e92b0e99fd5616f6517
Eduardo Moraes Comerlato <eduardomc94@gmail.com>
2021-01-26 13:16:21 -0300

    Aula 19

--- src/main/java/duds/springbootessentials/service/AnimeService.java
+++ src/main/java/duds/springbootessentials/service/AnimeService.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -31,7 +32,7 @@ public class AnimeService {
         return animeRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException("Anime not Found"));
     }
-
+    @Transactional //EM CASO DE EXCEÇÃO, TRANSACTIONAL GARANTIRÁ ROLLBACK DA OPERAÇÃO
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
         //return animeRepository.save(Anime.builder().name(animePostRequestBody.getName()).build());
