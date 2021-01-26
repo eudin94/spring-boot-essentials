dc6eb5cef73c5a92ac1ecc2ef73994040fb6fe4a
Eduardo Moraes Comerlato <eduardomc94@gmail.com>
2021-01-26 11:20:01 -0300

    Aula 17

--- src/main/java/duds/springbootessentials/service/AnimeService.java
+++ src/main/java/duds/springbootessentials/service/AnimeService.java
@@ -1,6 +1,7 @@
 package duds.springbootessentials.service;
 
 import duds.springbootessentials.domain.Anime;
+import duds.springbootessentials.exception.BadRequestException;
 import duds.springbootessentials.mapper.AnimeMapper;
 import duds.springbootessentials.repository.AnimeRepository;
 import duds.springbootessentials.requests.AnimePostRequestBody;
@@ -28,7 +29,7 @@ public class AnimeService {
 
     public Anime findByIdOrThrowBadRequestException(long id) {
         return animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+                .orElseThrow(() -> new BadRequestException("Anime not Found"));
     }
 
     public Anime save(AnimePostRequestBody animePostRequestBody) {
