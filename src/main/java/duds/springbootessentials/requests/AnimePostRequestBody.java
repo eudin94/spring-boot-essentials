1f163d7cdbc375d32715ecdfe5a2f99020d46e6a
Eduardo Moraes Comerlato <eduardomc94@gmail.com>
2021-01-26 13:26:46 -0300

    Aula 20

--- src/main/java/duds/springbootessentials/requests/AnimePostRequestBody.java
+++ src/main/java/duds/springbootessentials/requests/AnimePostRequestBody.java
@@ -2,7 +2,11 @@ package duds.springbootessentials.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The anime name cannot be empty")
+//    @NotNull(message = "The anime name cannot be null") EMPTY WORKS FOR NULL
     private String name;
 }
