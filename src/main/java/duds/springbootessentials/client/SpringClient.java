ff689d81f846c3a2af00a64ab2074305c0dd4b2f
Eduardo Moraes Comerlato <eduardomc94@gmail.com>
2021-01-27 14:48:35 -0300

    Aula 29

--- src/main/java/duds/springbootessentials/client/SpringClient.java
+++ src/main/java/duds/springbootessentials/client/SpringClient.java
@@ -33,7 +33,20 @@ public class SpringClient {
         ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
                 HttpMethod.POST, new HttpEntity<>(samuraiChamploo, createJsonHeader()), Anime.class);
         log.info("saved anime {}", samuraiChamplooSaved);
+
+        Anime animeToBeUpdated = samuraiChamplooSaved.getBody();
+        animeToBeUpdated.setName("Samurai Champloo 2");
+
+        ResponseEntity<Void> samuraiChamplooUpdated = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.PUT, new HttpEntity<>(animeToBeUpdated, createJsonHeader()), Void.class);
+        log.info(samuraiChamplooUpdated);
+
+        ResponseEntity<Void> samuraiChamplooDeleted = new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+                HttpMethod.DELETE, null, Void.class, animeToBeUpdated.getId());
+        log.info(samuraiChamplooUpdated);
     }
+
+
 
     private static HttpHeaders createJsonHeader() {
         HttpHeaders httpHeaders = new HttpHeaders();
