23263b95fb3a59e25c6960d66766fe1c19c9ab14
Eduardo Moraes Comerlato <eduardomc94@gmail.com>
2021-01-27 11:50:10 -0300

    Aula 27

--- src/main/java/duds/springbootessentials/client/SpringClient.java
+++ src/main/java/duds/springbootessentials/client/SpringClient.java
@@ -2,8 +2,13 @@ package duds.springbootessentials.client;
 
 import duds.springbootessentials.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -13,5 +18,12 @@ public class SpringClient {
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/6", Anime.class);
         log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+        log.info(Arrays.toString(animes));
+
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {});
+        log.info(exchange.getBody());
     }
 }
--- src/main/java/duds/springbootessentials/service/AnimeService.java
+++ src/main/java/duds/springbootessentials/service/AnimeService.java
@@ -26,6 +26,10 @@ public class AnimeService {
         return animeRepository.findAll(pageable);
     }
 
+    public List<Anime> listAllNonPageable() {
+        return animeRepository.findAll();
+    }
+
     public List<Anime> findByName(String name) {
         return animeRepository.findByName(name);
     }
@@ -54,4 +58,5 @@ public class AnimeService {
                 .build();   */
         animeRepository.save(anime);
     }
+
 }
